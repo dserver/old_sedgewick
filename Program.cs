4253a884cf88d8fd284454d6d2f8799555f0c284
unknown <Bswift@BSWIFT3.gro.Doncasters.com>
2015-03-23 14:11:51 -0400

    added rnd number gen to main method and notes for how to compile

--- Program.cs
+++ Program.cs
@@ -1,26 +1,62 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+//using System.Threading.Tasks;
 
-
+// Use 3.5 on work computer to compile. do:
+// set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework\v3.5
+// then csc.exe Program.cs; ./Program.exe + whatever classes you need
 namespace ConsoleApplication4
 {
     class Program
     {
         static void Main(string[] args)
         {
-            ElemArrayList<int> b = new ElemArrayList<int>();
-            int x;
-            for (x = 0; x <= 9; x++)
-            {
-                b.insert(x+1,x);
-            }
-
-            b.insert(11, 11);
-
-            int y = 1;
+			int seed = 100100;
+			Random rnd = new Random(seed);
+			
+			ArrayQueue<double> arrayq = new ArrayQueue<double>();
+			int i;
+			for (i=0;i<100;i++){
+				arrayq.add(rnd.NextDouble());
+			}
         }
+		
+		static void readFromFile(string filename){
+			int counter = 0;
+			string line;
+
+			// Read the file and display it line by line.
+			System.IO.StreamReader file = 
+			   new System.IO.StreamReader(filename);
+			while((line = file.ReadLine()) != null)
+			{
+			   Console.WriteLine (line);
+			   counter++;
+			}
+
+			file.Close();
+		}
+		
+		static void parseSites(string filename){
+			int counter = 0;
+			string line;
+			// Read the file and display it line by line.
+			System.IO.StreamReader file = 
+			   new System.IO.StreamReader(filename);
+			while((line = file.ReadLine()) != null)
+			{
+			   line.Split(' ');
+			   Console.Write(line[0] + " " + line[1]);
+			   Console.WriteLine();
+			   counter++;
+			}
+
+			file.Close();
+			
+			
+		}
     }
 }
